ae88c90d9b425fedd74ce7875081520c0e597463
Rajas Kakodkar <rkakodkar@vmware.com>
2022-03-01 01:16:21 +0530

    fix base build job
    Signed-off-by: Rajas Kakodkar <rkakodkar@vmware.com>

--- scripts/ci-docker-build-image.sh
+++ scripts/ci-docker-build-image.sh
@@ -10,8 +10,25 @@ main() {
   registry_path="${2}"
 
   # Start Docker
-  /usr/local/bin/dockerd-entrypoint.sh &
-
+  printf '=%.0s' {1..80}; echo
+  service docker start
+  # the service can be started but the docker socket not ready, wait for ready
+  WAIT_N=0
+  MAX_WAIT=5
+  while true; do
+    # docker ps -q should only work if the daemon is ready
+    docker ps -q > /dev/null 2>&1 && break
+    if [[ ${WAIT_N} -lt ${MAX_WAIT} ]]; then
+        WAIT_N=$((WAIT_N+1))
+        echo "Waiting for docker to be ready, sleeping for ${WAIT_N} seconds."
+        sleep ${WAIT_N}
+    else
+        echo "Reached maximum attempts, not waiting any longer..."
+        break
+    fi
+  done
+  printf '=%.0s' {1..80}; echo
+  echo "Done setting up docker in docker."
   # Docker takes a few seconds to initialize
   while (! docker stats --no-stream > /dev/null 2>&1 ); do
     sleep 1
